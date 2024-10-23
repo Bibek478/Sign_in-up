55092778ced9858dcebb0a232f09980bd385f8be
BIBEK <bibekroy478@gmail.com>
2024-10-23 20:43:50 +0530

    change Bg img

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// Selecting elements from the DOM
 let signUpBtn = document.querySelector('.signupbtn');
 let signInBtn = document.querySelector('.signinbtn');
 let nameField = document.querySelector('.namefield');
 let title = document.querySelector('.title');
 let underline = document.querySelector('.underline');
 let text = document.querySelector('.text');
+
+// Initial setup to display the Sign Up form
+nameField.style.maxHeight = '60px'; // Show the name field for Sign Up
+text.innerHTML = 'Password Suggestion'; // Set initial text
 
 // Event listener for Sign In button
 signInBtn.addEventListener('click', () => {
@@ -13,7 +16,7 @@ signInBtn.addEventListener('click', () => {
     text.innerHTML = 'Forgot Password'; // Change text to Forgot Password
     signUpBtn.classList.add('disable'); // Disable the Sign Up button
     signInBtn.classList.remove('disable'); // Enable the Sign In button
-    underline.style.transform = 'translateX(100px)'; // Move the underline to align with Sign In
+    underline.style.transform = 'translateX(100px)'; // Move underline to Sign In
 });
 
 // Event listener for Sign Up button
@@ -23,5 +26,5 @@ signUpBtn.addEventListener('click', () => {
     text.innerHTML = 'Password Suggestion'; // Change text to Password Suggestion
     signUpBtn.classList.remove('disable'); // Enable the Sign Up button
     signInBtn.classList.add('disable'); // Disable the Sign In button
-    underline.style.transform = 'translateX(0)'; // Move the underline back to Sign Up
+    underline.style.transform = 'translateX(0)'; // Move underline back to Sign Up
 });
